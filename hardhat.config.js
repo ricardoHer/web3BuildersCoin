f6a970945eba6b37dd29fcc27d1efe92fe769329
Ricardo FRanco Hernandez <ricardo.hern@icloud.com>
2024-11-28 11:35:02 -0300

    progress with ignition

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,10 +10,10 @@ module.exports = {
     hardhat: {
     },
     sepolia: {
-      url: "https://rpc.sepolia.org",
+      url: "https://eth-sepolia.g.alchemy.com/v2/Fmyf_pl7WZemB2FNg_wLl6Hza47tZpAW",
       accounts: [process.env.PRIVATE_KEY1],
-      // gas: 3000000, // Gas limit
-      // gasPrice: 30000000000, // 30 Gwei
+      gas: 3000000, // Gas limit
+      gasPrice: 30000000000, // 30 Gwei
     }
   },
 };
--- ignition/modules/Web3buildersCoin.js
+++ ignition/modules/Web3buildersCoin.js
@@ -6,15 +6,11 @@ const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 const { ethers } = require("hardhat");
 
 module.exports = buildModule("Web3Builder", (m) => {
-    const name = m.getParameter("name", "Web3Builder");
-    const symbol = m.getParameter("symbol", "WE3");
+    const name = m.getParameter("name", "Web3BuilderRickFranco");
+    const symbol = m.getParameter("symbol", "WR3");
     const initialSupply = m.getParameter("initialSupply", 1000000);
-    
-    // ethers.provider.getBalance("0x224057fbef1379d5BFa371ee778A6a5982990E82")
-    // .then(balance => console.log("Balance: ", balance.toString()));
 
-    const token = m.contract("Web3Builder", [name, symbol, initialSupply]);
-    
+    const token = m.contract("Web3Builder", [name, symbol, initialSupply]);    
     console.log(`Deployed ${name} with symbol ${symbol} and initial supply ${initialSupply}: ${token.address}`);
 
     return { token };
